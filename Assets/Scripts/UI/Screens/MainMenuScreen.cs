5fb961c8f394214fd5256a2e4c7b8329c9b73c1e
nicholasgarnerviad-debug <nicholas.garner.viad@gmail.com>
2026-05-22 05:20:17 -0400

    feat: create MainMenuScreen for mode selection

--- Assets/Scripts/UI/Screens/MainMenuScreen.cs
+++ Assets/Scripts/UI/Screens/MainMenuScreen.cs
@@ -1,114 +1,34 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using WordPuzzleGame.UI.Animations;
 
-public class MainMenuScreen : MonoBehaviour
+namespace WordPuzzle.UI
 {
-    [SerializeField] private Button classicModeButton;
-    [SerializeField] private Button puzzleShowButton;
-    [SerializeField] private Button timeAttackButton;
-    [SerializeField] private Button shopButton;
-    [SerializeField] private Button settingsButton;
-
-    private ModeController modeController;
-    private UIManager uiManager;
-
-    public void InjectDependencies(ModeController mc, UIManager ui)
+    public class MainMenuScreen : MonoBehaviour
     {
-        modeController = mc;
-        uiManager = ui;
-        UpdateButtonListeners();
-    }
+        [SerializeField] private Button classicModeButton;
+        [SerializeField] private Button puzzleShowButton;
+        [SerializeField] private Button timeAttackButton;
 
-    private void UpdateButtonListeners()
-    {
-        // Unsubscribe from previous listeners
-        if (classicModeButton != null) classicModeButton.onClick.RemoveAllListeners();
-        if (puzzleShowButton != null) puzzleShowButton.onClick.RemoveAllListeners();
-        if (timeAttackButton != null) timeAttackButton.onClick.RemoveAllListeners();
-        if (shopButton != null) shopButton.onClick.RemoveAllListeners();
-        if (settingsButton != null) settingsButton.onClick.RemoveAllListeners();
+        public event Action OnClassicModeSelected;
+        public event Action OnPuzzleShowSelected;
+        public event Action OnTimeAttackSelected;
 
-        // Subscribe to mode buttons
-        if (classicModeButton != null)
-            classicModeButton.onClick.AddListener(() => StartMode(ModeType.Classic));
-        else
-            Debug.LogWarning("[MainMenu] Classic mode button not assigned");
-
-        if (puzzleShowButton != null)
-            puzzleShowButton.onClick.AddListener(() => StartMode(ModeType.PuzzleShow));
-        else
-            Debug.LogWarning("[MainMenu] Puzzle show button not assigned");
-
-        if (timeAttackButton != null)
-            timeAttackButton.onClick.AddListener(() => StartMode(ModeType.TimeAttack));
-        else
-            Debug.LogWarning("[MainMenu] Time attack button not assigned");
-
-        // Subscribe to utility buttons
-        if (shopButton != null)
-            shopButton.onClick.AddListener(() => Debug.Log("Shop coming soon"));
-
-        if (settingsButton != null)
-            settingsButton.onClick.AddListener(() => Debug.Log("Settings coming soon"));
-    }
-
-    private void StartMode(ModeType modeType)
-    {
-        if (modeController == null)
+        private void OnEnable()
         {
-            Debug.LogWarning("[MainMenuScreen] ModeController not injected");
-            return;
+            classicModeButton.onClick.AddListener(() => OnClassicModeSelected?.Invoke());
+            puzzleShowButton.onClick.AddListener(() => OnPuzzleShowSelected?.Invoke());
+            timeAttackButton.onClick.AddListener(() => OnTimeAttackSelected?.Invoke());
         }
 
-        Debug.Log($"[MainMenu] Starting {modeType}");
-
-        // Play tap animation on clicked button
-        Button clickedButton = GetButtonForMode(modeType);
-        if (clickedButton != null)
+        private void OnDisable()
         {
-            RectTransform rectTransform = clickedButton.GetComponent<RectTransform>();
-            if (rectTransform != null)
-                StartCoroutine(UIAnimations.ScaleButtonTap(rectTransform));
+            classicModeButton.onClick.RemoveAllListeners();
+            puzzleShowButton.onClick.RemoveAllListeners();
+            timeAttackButton.onClick.RemoveAllListeners();
         }
 
-        // Switch mode
-        modeController.SwitchMode(modeType);
-
-        // Animate out and show gameplay
-        StartCoroutine(TransitionToGameplay());
-    }
-
-    private Button GetButtonForMode(ModeType modeType)
-    {
-        return modeType switch
-        {
-            ModeType.Classic => classicModeButton,
-            ModeType.PuzzleShow => puzzleShowButton,
-            ModeType.TimeAttack => timeAttackButton,
-            _ => null
-        };
-    }
-
-    private IEnumerator TransitionToGameplay()
-    {
-        // Wait for button tap animation to complete
-        yield return new WaitForSeconds(0.1f);
-
-        // Get CanvasGroup for fade out
-        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
-        if (canvasGroup != null)
-        {
-            yield return StartCoroutine(UIAnimations.FadeTransition(canvasGroup, false, 0.3f));
-            canvasGroup.alpha = 0;
-        }
-
-        // Show gameplay screen
-        uiManager.ShowScreen<GameplayScreen>();
-
-        // Reset for next menu visit
-        if (canvasGroup != null)
-            canvasGroup.alpha = 1f;
+        public void Show() => gameObject.SetActive(true);
+        public void Hide() => gameObject.SetActive(false);
     }
 }
